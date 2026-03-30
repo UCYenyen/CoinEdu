7edb058987637470107adfb419e3bb1683221f9c
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-31 01:02:37 +0700

    fix auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,6 +11,7 @@ export const auth = betterAuth({
     provider: "postgresql", 
   }),
   baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [process.env.BETTER_AUTH_URL || ""].filter(Boolean),
   
   emailAndPassword: {
     enabled: true,
